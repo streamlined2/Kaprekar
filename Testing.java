9fe0b48ee34781ae3dee84ec69f7ddf36f260416
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2020-11-15 12:20:59 +0200

    Add files via upload
    Final shot.
    Corrected 2 mistakes:
    1.  += replaced with = in line
    value=value*10+digit;
    of intValue method;
    
    2. extracted digit placed as first node in list, not appended to the tail
    digits.add(0,value%10);
    in digits method.

--- Testing.java
+++ Testing.java
@@ -1,45 +1,54 @@
-// Put any necessary java8 imports you may need for solution
 import java.util.*;
 
 public class Testing {
+
     public final static int MAGIC_NUMBER=6174;
-	public static int kaprekar(int num) {
+
+    public static void main(String[] args) {
+    	System.out.printf("Steps to reach kaprekar number %d: %d\n",6162,kaprekar(6162));
+    	System.out.printf("Steps to reach kaprekar number %d: %d\n",2345,kaprekar(2345));
+    	System.out.printf("Steps to reach kaprekar number %d: %d\n",6734,kaprekar(6734));
+    	System.out.printf("Steps to reach kaprekar number %d: %d\n",8934,kaprekar(8934));
+    	System.out.printf("Steps to reach kaprekar number %d: %d\n",3278,kaprekar(3278));
+	}
+
+    public static int kaprekar(final int num) {
 	    int count=0;
 	    int number=num;
-	    int first,second;
 	    do{
-	        first=firstNumber(number);
-	        second=secondNumber(number);
-	        number=first-second;
+	        number=firstNumber(number)-secondNumber(number);
 	        count++;
 	    }while(number!=MAGIC_NUMBER);
 	    return count;
   }
-  public static int firstNumber(int num){
-      List<Integer> digits=digits(num);
+    
+  public static int firstNumber(final int num){
+      final List<Integer> digits=digits(num);
       Collections.sort(digits,Comparator.reverseOrder());
       return intValue(digits);
   }
   
-  public static int secondNumber(int num){
-      List<Integer> digits=digits(num);
+  public static int secondNumber(final int num){
+      final List<Integer> digits=digits(num);
       Collections.sort(digits);
       return intValue(digits);
   }
   
-  public static int intValue(List<Integer> digits){
+  public static int intValue(final List<Integer> digits){
       int value=0;
       for(int digit:digits){
-          value+=value*10+digit;
+          value=value*10+digit;
       }
       return value;
   }
   
-  public static ArrayList<Integer> digits(int num){
-      ArrayList<Integer> digits=new ArrayList<>();
+  public static List<Integer> digits(final int num){
+      final List<Integer> digits=new ArrayList<>();
       for(int value=num;value>0;value=value/10){
-          digits.add(value%10);
+          digits.add(0,value%10);
       }
       return digits;
   }
+
+ 
 }
